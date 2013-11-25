d656d6c7653353a20f118ead9c4ee2d7f18bd447
Theo Armour <t.armour@gmail.com>
2013-11-24 17:23:40 -0800

    updates

--- db/r2/doc-browser.js
+++ db/r2/doc-browser.js
@@ -4,7 +4,7 @@
 	DS.css.innerHTML = 'body { font: bold 12pt monospace; margin: 0; overflow: hidden; }' +
 		'h1, h2, h3, p { margin: 10px 0px; padding: 0px 25px;  }' +
 		'p {padding: 5px 20px 5px 25px; }' +
-		'#githubio { display: block important!; }' +
+		'iframe #overview { border: red solid 3px; opacity: 0.2; }' +
 	'';	
 
 	DS.converter = new Showdown.converter();	
@@ -24,9 +24,13 @@
 	
 	DS.menu = document.body.appendChild( document.createElement( 'div' ) );
 	DS.menu.style.cssText = DS.basic + DS.height + DS.horzMnu + DS.top;
+	
+	DS.watermark = document.body.appendChild( document.createElement( 'div' ) );
+	DS.watermark.style.cssText = DS.basic + DS.height + DS.horzRdm + DS.top + 'font-size: 48pt; margin-top: 200px; opacity: 0.08; text-align:center; transform:rotate(7deg); -webkit-transform:rotate(-10deg);';	
 
 	DS.readme = document.body.appendChild( document.createElement( 'div' ) );
-	DS.readme.style.cssText = DS.basic + DS.height + DS.horzRdm + DS.top;
+	DS.readme.style.cssText = DS.basic + DS.height + DS.horzRdm + DS.top ;
+	
 	
 	DS.editButton = document.createElement( 'div' ) ;
 	DS.editButton.style.cssText = 'background-color: #555; color: #fff; opacity: 0.5; padding: 8px; position: absolute; right: 5%; top: 3%; ';
@@ -54,6 +58,12 @@
 		
 		DS.readme.appendChild( DS.editButton );
 		DS.readme.children[2].style.cssText = 'display:block;'
+		
+		if ( window.frames.length > 0 ) {
+			DS.watermark.innerHTML = 'Working Sample';
+		} else {
+			DS.watermark.innerHTML = '';
+		}		
 		
 		DS.clearMenuHighlights( element );
 
